d1336ea6acbeb23faf440d0fd98da9e3ed55b65e
Riyad Alalami <riyadhelalami@gmail.com>
2021-02-08 00:58:52 -0500

    Fixed some build issues

--- Core/Src/menu.cpp
+++ Core/Src/menu.cpp
@@ -316,3 +316,12 @@ void menu::keyboard_poll()
     GPIOD->ODR&=(~pin);
 
 }
+
+void menu::menu_up()//TOOD:implement up button
+{
+
+}
+void menu::menu_down()//TODO:implement down button
+{
+    
+}
